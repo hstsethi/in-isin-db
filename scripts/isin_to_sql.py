eeca5c9bf39737e0f8b454cbbeb19a7e327e93c6
HstSethi <107557004+hstsethi@users.noreply.github.com>
2025-10-23 14:07:02 +0530

    Add CHECK constraint

--- scripts/isin_to_sql.py
+++ scripts/isin_to_sql.py
@@ -1,7 +1,8 @@
 import sqlite3
 import pandas as pd
 def create_table(con):
-    con.execute('''CREATE TABLE IF NOT EXISTS isin(isin char(12) PRIMARY KEY, symbol NOT NULL);''')
+    con.execute('''CREATE TABLE IF NOT EXISTS isin(isin char(12) PRIMARY KEY, symbol NOT NULL 
+  CHECK (length(isin) = 12);''')
     con.commit()
 
 def main():
