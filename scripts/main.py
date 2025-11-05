00e66a7e68068d88d7bc5f0a274961b07c0e482f
Harshit Sethi <107557004+hstsethi@users.noreply.github.com>
2025-11-05 18:44:16 +0530

    Support SME segment

--- scripts/main.py
+++ scripts/main.py
@@ -1,11 +1,29 @@
-from clean_nse_csv import cleanColumns
-from download_nse_csv import downloadEquityCsv
+from clean_nse_csv import clean_columns
+from download_nse_csv import download_nse_CSV
+
+
+def setup_SME():
+    SME_list_dataset = download_nse_CSV(
+        "../data/nse-sme.csv",
+        "https://nsearchives.nseindia.com/content/equities/SME_EQUITY_L.csv",
+    )
+    clean_SME_dataset = clean_columns(SME_list_dataset)
+    clean_SME_dataset.to_csv(SME_list_dataset, index=False)
+
+
+def setup_equity():
+    sec_list_dataset = download_nse_CSV(
+        "../data/nse-equity.csv",
+        "https://nsearchives.nseindia.com/content/equities/EQUITY_L.csv",
+    )
+
+    clean_dataset = clean_columns(sec_list_dataset)
+    clean_dataset.to_csv(sec_list_dataset, index=False)
 
 
 def main():
-    sec_list_dataset = downloadEquityCsv()
-    clean_dataset = cleanColumns(sec_list_dataset)
-    clean_dataset.to_csv(sec_list_dataset, index=False)
+    setup_SME()
+    setup_equity()
     return 0
 
 
